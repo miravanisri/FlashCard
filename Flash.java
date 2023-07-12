954ed9547d43203092ef7d0dac9b800d0ff4ae64
miravanisri <107063870+miravanisri@users.noreply.github.com>
2023-07-12 19:13:34 +0530

    Create Flash.java

---
+++ Flash.java
@@ -0,0 +1,40 @@
+package Flash.java;
+
+public class FlashCard {
+private String q;
+private String ans;
+
+public FlashCard(String q,String a)
+{
+	this.q=q;
+	this.ans=a;
+	
+}
+
+public String getQ() {
+	return q;
+}
+
+
+public void setQ(String q) {
+	this.q = q;
+}
+
+
+public String getAns() {
+	return ans;
+}
+
+
+public void setAns(String ans) {
+	this.ans = ans;
+}
+
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
